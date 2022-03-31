19e131a40f77c14ad7a48ae588bec286ec00b147
JeffAllison <itsjallison@gmail.com>
2022-03-31 11:57:22 -0700

    imported functions from fetch.js and added workshopsEl from workshops.html

--- workshops/workshops.js
+++ workshops/workshops.js
@@ -1,7 +1,12 @@
-import { checkAuth, logout } from '../fetch-utils.js';
+import {
+  checkAuth,
+  logout,
+  deleteParticipant,
+  getWorkshops
+} from '../fetch-utils.js';
 
 checkAuth();
-
+const workshopsEl = document.querySelector('.workshops-container');
 const logoutButton = document.getElementById('logout');
 
 logoutButton.addEventListener('click', () => {
