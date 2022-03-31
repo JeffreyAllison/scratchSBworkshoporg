2b2329ec80e6dbf9be924be7c0d01a81376ae4b2
JeffAllison <itsjallison@gmail.com>
2022-03-31 12:13:11 -0700

    added function fetchAndDisplayWorkshops and window event listner to display workshops on load

--- workshops/workshops.js
+++ workshops/workshops.js
@@ -6,9 +6,51 @@ import {
 } from '../fetch-utils.js';
 
 checkAuth();
+
 const workshopsEl = document.querySelector('.workshops-container');
 const logoutButton = document.getElementById('logout');
 
 logoutButton.addEventListener('click', () => {
   logout();
+});
+
+async function fetchAndDisplayFamilies () {
+  const workshops = await getWorkshops();
+  workshopsEl.textContent = '';
+
+  for (let workshop of workshops) {
+    const workshopEl = document.createElement('div');
+    const nameEl = document.createElement('h4');
+    const participantsEl = document.createElement('div');
+
+    participantsEl.classList.add('participants');
+    workshopEl.classList.add('workshop');
+
+    nameEl.textContent = workshop.name;
+
+    for (let participant of workshop.participants) {
+      const participantEl = document.createElement('div');
+
+      participantEl.classList.add('participant');
+      participantEl.textContent = participant.name;
+
+      participantEl.addEventListener('click', async () => {
+        await deleteParticipant(participant.id);
+
+        const updatedWorkshops = await getWorkshops();
+
+        fetchAndDisplayFamilies(updatedWorkshops);
+
+      });
+      participantsEl.append(participantEl);
+    }
+    workshopEl.append(nameEl, participantsEl);
+    workshopsEl.append(workshopEl);
+  }
+}
+
+window.addEventListener('load', async () => {
+  const workshops = await getWorkshops();
+
+  fetchAndDisplayFamilies(workshops);
 });
