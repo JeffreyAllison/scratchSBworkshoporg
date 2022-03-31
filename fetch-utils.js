8fde29ecddc18b49db6a0de4b48b943c85fc6b7a
JeffAllison <itsjallison@gmail.com>
2022-03-31 11:51:29 -0700

    added supabase URL and key, added functions getWorkshops, deleteParticipant, and createParticipant in fetch.js

--- fetch-utils.js
+++ fetch-utils.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 const SUPABASE_URL = 'https://lrbzhpldjrxqkjskcizc.supabase.co';
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxyYnpocGxkanJ4cWtqc2tjaXpjIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDc1NTIwMDMsImV4cCI6MTk2MzEyODAwM30.idE1m2ehmckSIic7mOSaXFl1McMzBdIrhU_Vrsr6UyI';
 
@@ -5,6 +6,35 @@ const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
 export function getUser () {
   return client.auth.session() && client.auth.session().user;
+}
+
+export async function getWorkshops () {
+  const response = await client
+    .from('workshops')
+    .select('*, participants (*)');
+
+  return checkError(response);
+}
+
+export async function deleteParticipant (id) {
+  const response = await client
+    .from('participants')
+    .delete()
+    .match({ id: id });
+
+  return checkError(response);
+}
+
+export async function createParticipant (participant) {
+  const response = await client
+    .from('participants')
+    .insert({
+      name: participant.name,
+      workshop_id: participant.workshop_id,
+      user_id: client.auth.user().id
+    });
+
+  return checkError(response);
 }
 
 export function checkAuth () {
@@ -15,7 +45,7 @@ export function checkAuth () {
 
 export function redirectIfLoggedIn () {
   if (getUser()) {
-    location.replace('./other-page');
+    location.replace('./workshops');
   }
 }
 
@@ -37,6 +67,6 @@ export async function logout () {
   return (window.location.href = '../');
 }
 
-// function checkError({ data, error }) {
-//     return error ? console.error(error) : data;
-// }
+function checkError ({ data, error }) {
+  return error ? console.error(error) : data;
+}
